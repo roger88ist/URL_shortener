aa940af2df233d7e64e0c79400d3eeda35813638
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-07 11:49:43 -0400

    added create action in controller

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -6,7 +6,18 @@ class WebsitesController < ApplicationController
   def new
   end
 
+  def show
+  end
+
   def create
-  	@
+  	@website = Website.new
+  	array = ("a".."z").map {|letter| letter}
+  	@website.short = array.sample(4).join
+
+  	if @Website.save
+  		websites_path
+  	else
+  		render 'new'
+  	end
   end
 end
