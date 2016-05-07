af691f722b5875fab4e290483b23131bbcc768c5
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-07 12:37:28 -0400

    white listed params in private methods in controller file

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -4,20 +4,29 @@ class WebsitesController < ApplicationController
   end
 
   def new
-  end
-
-  def show
+  	@website = Website.new
   end
 
   def create
-  	@website = Website.new
+  	@website = Website.new(website_params)
   	array = ("a".."z").map {|letter| letter}
   	@website.short = array.sample(4).join
 
-  	if @Website.save
-  		websites_path
+  	if @website.save
+  		redirect_to websites_path
   	else
   		render 'new'
   	end
   end
+
+  def show
+  	@website = Website.find(params[:id])
+  end
+
+  private
+
+  def website_params
+  	params.require(:website).permit(:address)
+  end
+  
 end
